d654c287b302ade29103fa5309979f74e366a39d
minnily <156267800+minnily@users.noreply.github.com>
2024-04-24 21:50:26 +0900

    4.24

--- boardProject-boot/src/main/java/edu/kh/project/board/controller/BoardController.java
+++ boardProject-boot/src/main/java/edu/kh/project/board/controller/BoardController.java
@@ -1,5 +1,8 @@
 package edu.kh.project.board.controller;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,13 +10,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import edu.kh.project.board.model.dto.Board;
 import edu.kh.project.board.model.dto.BoardImg;
 import edu.kh.project.board.model.service.BoardService;
+import edu.kh.project.member.model.dto.Member;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -61,7 +72,10 @@ public class BoardController {
 	// /board/1/1960?cp=2
 	
 	@GetMapping("{boardCode:[0-9]+}/{boardNo:[0-9]+}")
-	public String boardDetail(@PathVariable("boardCode") int boardCode, @PathVariable("boardNo") int boardNo, Model model, RedirectAttributes ra) {
+	public String boardDetail(@PathVariable("boardCode") int boardCode, @PathVariable("boardNo") int boardNo, Model model, RedirectAttributes ra,
+		@SessionAttribute(value = "loginMember", required = false) Member loginMember,
+		HttpServletRequest req, //요청에 담긴 쿠키 얻어오기
+		HttpServletResponse resp ) { // 새로운 쿠키 만들어서 응답하기) {
 		
 		// 게시글 상세 조회 서비스 호출
 		
@@ -69,6 +83,11 @@ public class BoardController {
 		Map<String, Integer> map = new HashMap<>();
 		map.put("boardCode",boardCode);
 		map.put("boardNo",boardNo);
+		
+		//로그인 상태인 경우에만 memberNo 추가
+		if(loginMember != null) {
+			map.put("memberNo", loginMember.getMemberNo());
+		}
 		
 		// 2) 서비스 호출
 		Board board = service.selectOne(map);
@@ -81,6 +100,83 @@ public class BoardController {
 			
 		//조회 결과가 있는 경우
 		}else {
+			
+			/********************* 쿠키를 이용한 조회수 증가(시작)*********************/
+			
+			// 1. 비회원 또는 로그인한 회원의 글이 아닌 경우
+			// (글쓴이를 뺀 다른 사람)
+			
+			if(loginMember == null || loginMember.getMemberNo() != board.getMemberNo()) {
+			
+				// 요청에 담겨있는 모든 쿠키 얻어오기
+				Cookie[] cookies = req.getCookies();
+				
+				Cookie c = null;
+				
+				for(Cookie temp : cookies) {
+					//요청에 담긴 쿠키에 "readBoardNo"가 존재 할 때
+					if(temp.getName().equals("readBoardNo")) {
+						c = temp;
+						break;
+					}
+					
+				}
+				
+				int result = 0; // 조회수 증가 결과를 저장할 변수
+				// "readBoardNo"가 쿠키에 없을 때 
+				if(c == null) {
+					
+					// 새 쿠키 생성 ("readBoardNo", [게시글 번호])
+					c = new Cookie("readBoardNo", "[" + boardNo + "]");
+					result = service.updateReadCount(boardNo);
+					
+					
+				}else {
+					// "readBoardNo"가 쿠키에 있을 때
+					// "readBoardNo" : [2][30][200][4000]
+					
+					// 현재 게시글을 처음 읽은 경우
+					if(c.getValue().indexOf("["+ boardNo + "]") == -1 ){
+					
+						// 해당 글 번호를 쿠키에 누적 + 서비스 호출
+						c.setValue(c.getValue() + "["+ boardNo + "]");
+						result = service.updateReadCount(boardNo);
+					}
+					
+				}
+				
+				
+				// 조회 수 증가 성공/ 조회 성공
+				if(result > 0) {
+				
+					// 먼저 조회된 board의 readCount 값을 result 값으로 변환(재대입)
+					board.setReadCount(result);
+				
+					// 적용 경로 설정 
+					c.setPath("/"); // "/" 이하 경로 요청 시 쿠키 서버로 전달 
+				
+					// 쿠키의 수명 지정
+					
+					// 현재 시간을 얻어오기
+					LocalDateTime now = LocalDateTime.now();
+					
+					// 다음날 자정
+					LocalDateTime nextDayMidnight = now.plusDays(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
+					
+					// 다음날 자정까지 남은 시간 계산 (초단위)
+					long secondsUntilNextDay = Duration.between(now, nextDayMidnight).getSeconds();
+					
+					// 쿠키 수명 설정
+					c.setMaxAge((int)secondsUntilNextDay);
+					
+					resp.addCookie(c); //응답객체를 이용해서 클라이언트에게 전달
+				}
+				
+				
+			}
+			
+			/********************* 쿠키를 이용한 조회수 증가(끝) **********************/
+			
 			path = "board/boardDetail"; // board/boadrDetail.html로 forward
 			
 			// board 안에서는 게시글 일반 내용 + imageList +commentList가 들어가 있음
@@ -105,7 +201,19 @@ public class BoardController {
 			
 		}
 		
-		return path; 
-		
+		return path; 	
 	}
+	
+	/** 게시글 좋아요 체크/해제
+	 * @param map
+	 * @return count 
+	 */
+	@ResponseBody
+	@PostMapping("like")
+	public int boardLike(@RequestBody Map<String, Integer> map) {
+		
+		return service.boardLike(map);
+	}
+	
+	
 }
