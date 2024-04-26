975dc1e86761335f4e8a6f8a74b30de1b16ff1b2
minnily <156267800+minnily@users.noreply.github.com>
2024-04-26 17:44:27 +0900

    4.26

--- boardProject-boot/src/main/resources/static/js/board/boardDetail.js
+++ boardProject-boot/src/main/resources/static/js/board/boardDetail.js
@@ -54,3 +54,20 @@ document.querySelector("#boardLike").addEventListener("click", e => {
 
     })
 })
+
+//---------------------- 게시글 수정 버튼 -------------------------------------
+
+const updateBtn = document.querySelector("#updateBtn");
+
+if(updateBtn != null){ // 수정 버튼 존재 시 
+    updateBtn.addEventListener("click", () => {
+        
+        // GET 방식
+        // 현재 board/1/2001?cp=1
+        // 목표 : /editBoard/1/2001/update?cp=1
+        location.href = location.pathname.replace('board','editBoard')
+                        + "/update"
+                        + location.search;
+    })
+
+}
